606bf7a0909759dd6cb44ab9f759cbe6ec30ba89
Nelwill <nelwill@f-list.net>
2015-12-28 19:17:16 +0100

    Adds preliminary support for gender colors

--- f-list_pidgin.c
+++ f-list_pidgin.c
@@ -7,8 +7,13 @@
 #include "gtkutils.h"
 #include "pidginstock.h"
 
-//TODO: fix these up so they're only one function ...
 
+static void (*chat_add_users_orig)(PurpleConversation *conv, GList *cbuddies, gboolean new_arrivals) = NULL;
+static void (*chat_update_user_orig)(PurpleConversation *conv, const char *user) = NULL;
+static void (*write_conv_orig)(PurpleConversation *conv, const char *name, const char *alias,
+        const char *message, PurpleMessageFlags flags, time_t mtime) = NULL;
+
+//TODO: fix these up so they're only one function ...
 static gboolean flist_channel_activate_real(const gchar *host, const gchar *path) {
     PurpleAccount *pa;
     PurpleConnection *pc;
@@ -241,9 +246,146 @@ static void flist_conversation_created_cb(PurpleConversation *conv, FListAccount
     purple_conversation_set_data(conv, FLIST_CONV_ALERT_STAFF_BUTTON, alert_button);
 }
 
+static void flist_pidgin_set_user_gender_color(PurpleConversation *conv, PurpleConvChatBuddy *buddy) {
+    PurpleConnection *pc = purple_conversation_get_gc(conv);
+
+    // Is this a conversation of our account?
+    if (!purple_strequal(pc->account->protocol_id, FLIST_PLUGIN_ID))
+        return;
+
+    FListAccount *fla = pc->proto_data;
+    PidginConversation *gtkconv = PIDGIN_CONVERSATION(conv);
+    PidginChatPane *gtkchat = gtkconv->u.chat;
+
+    GtkTreeModel *tm = gtk_tree_view_get_model(GTK_TREE_VIEW(gtkchat->list));
+    GtkListStore *ls = GTK_LIST_STORE(tm);
+
+    GtkTreeIter iter;
+
+    GtkTreePath *path;
+
+    if (!buddy->ui_data || (path = gtk_tree_row_reference_get_path(buddy->ui_data)) == NULL)
+        return;
+
+    GdkColor color;
+    FListCharacter *character = flist_get_character(fla, buddy->name);
+    if (!gdk_color_parse(flist_gender_color(character->gender), &color))
+        return;
+
+    if (gtk_tree_model_get_iter(tm, &iter, path)) {
+        gtk_tree_path_free(path);
+        gtk_list_store_set(ls, &iter, CHAT_USERS_COLOR_COLUMN, &color, -1); 
+    }
+}
+
+static void flist_pidgin_conv_chat_add_users(PurpleConversation *conv, GList *cbuddies, gboolean new_arrivals) {
+    chat_add_users_orig(conv, cbuddies, new_arrivals);
+
+    PurpleConnection *pc = purple_conversation_get_gc(conv);
+
+    // Is this a conversation of our account?
+    if (!purple_strequal(pc->account->protocol_id, FLIST_PLUGIN_ID))
+        return;
+
+    GList *tmp = cbuddies;
+
+    PidginConversation *gtkconv = PIDGIN_CONVERSATION(conv);
+    GtkTextMark *mark = gtk_text_mark_new(NULL, TRUE);
+    GtkTextBuffer *text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(gtkconv->imhtml));
+    GtkTextIter end;
+    gtk_text_buffer_get_end_iter(text_buffer, &end);
+    gtk_text_buffer_add_mark(text_buffer, mark, &end);
+    while (tmp != NULL) {
+        PurpleConvChatBuddy *buddy = (PurpleConvChatBuddy*)tmp->data;
+        flist_pidgin_set_user_gender_color(conv, buddy);
+        write_conv_orig(conv, buddy->name, buddy->name, buddy->name, PURPLE_MESSAGE_RECV | PURPLE_MESSAGE_INVISIBLE, time(NULL));
+
+        tmp = tmp->next;
+    }
+    GtkTextIter start;
+    gtk_text_buffer_get_iter_at_mark(text_buffer, &start, mark);
+    gtk_text_buffer_delete_mark(text_buffer, mark);
+    g_free(mark);
+
+    gtk_text_buffer_get_end_iter(text_buffer, &end);
+    gtk_imhtml_delete(GTK_IMHTML(gtkconv->imhtml), &start, &end);
+}
+
+static void flist_pidgin_conv_chat_update_user(PurpleConversation *conv, const char *user) {
+    chat_update_user_orig(conv, user);
+
+    PurpleConnection *pc = purple_conversation_get_gc(conv);
+
+    // Is this a conversation of our account?
+    if (!purple_strequal(pc->account->protocol_id, FLIST_PLUGIN_ID))
+        return;
+
+    PurpleConvChatBuddy *buddy = purple_conv_chat_cb_find(PURPLE_CONV_CHAT(conv), user);
+
+    g_return_if_fail(buddy);
+
+    flist_pidgin_set_user_gender_color(conv, buddy);
+}
+
+static void flist_pidgin_conv_write_conv(PurpleConversation *conv, const char *name, const char *alias,
+        const char *message, PurpleMessageFlags flags, time_t mtime) {
+
+    write_conv_orig(conv, name, alias, message, flags, mtime);
+
+    PurpleConnection *pc = purple_conversation_get_gc(conv);
+
+    // Is this a conversation of our account?
+    if (!purple_strequal(pc->account->protocol_id, FLIST_PLUGIN_ID))
+        return;
+
+    FListAccount *fla = pc->proto_data;
+
+    PidginConversation *gtkconv = PIDGIN_CONVERSATION(conv);
+
+    GtkTextBuffer *buffer = GTK_IMHTML(gtkconv->imhtml)->text_buffer;
+    gchar *tag_name = g_strdup_printf("BUDDY %s", name);
+
+    GtkTextTag *buddy_tag = gtk_text_tag_table_lookup(
+                                gtk_text_buffer_get_tag_table(buffer), tag_name);
+
+    if (!buddy_tag)
+        return;
+
+    GdkColor color;
+    FListCharacter *character = flist_get_character(fla, name);
+    if (!character || !gdk_color_parse(flist_gender_color(character->gender), &color))
+        return;
+
+    g_object_set(G_OBJECT(buddy_tag), "foreground-set", TRUE, "foreground-gdk", &color, NULL);
+    g_free(tag_name);
+}
+
 void flist_pidgin_init() {
+
     gtk_imhtml_class_register_protocol("flistc", flist_channel_activate, NULL);
     gtk_imhtml_class_register_protocol("flistsfc", flist_staff_activate, NULL);
+
+    PurpleConversationUiOps *ui_ops = pidgin_conversations_get_conv_ui_ops();
+
+    if (!chat_add_users_orig) {
+        chat_add_users_orig = ui_ops->chat_add_users;
+        chat_update_user_orig = ui_ops->chat_update_user;
+        write_conv_orig = ui_ops->write_conv;
+    }
+
+    ui_ops->chat_add_users = flist_pidgin_conv_chat_add_users;
+    ui_ops->chat_update_user = flist_pidgin_conv_chat_update_user;
+    ui_ops->write_conv = flist_pidgin_conv_write_conv;
+}
+
+void flist_pidgin_terminate() {
+    gtk_imhtml_class_register_protocol("flistc", NULL, NULL);
+    gtk_imhtml_class_register_protocol("flistsfc", NULL, NULL);
+
+    PurpleConversationUiOps *ui_ops = pidgin_conversations_get_conv_ui_ops();
+    ui_ops->chat_add_users = chat_add_users_orig;
+    ui_ops->chat_update_user = chat_update_user_orig;
+    ui_ops->write_conv = write_conv_orig;
 }
 
 void flist_pidgin_enable_signals(FListAccount *fla)
