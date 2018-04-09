fchat-pidgin/fchat-pidgin

commit b9862d53ddb5f031d3ba6571308788126b3f6223

Fix an assert warning when fetching friend list

f-list_friends.c
@@ -512,7 +512,11 @@ static void flist_handle_friends_list(FListAccount *fla, JsonArray *array, FList
         JsonObject *o = json_array_get_object_element(array, index);
         const gchar *source = json_object_get_string_member(o, !in ? "source" : "dest");
         const gchar *dest = json_object_get_string_member(o, !in ? "dest" : "source");
-        gint id = json_object_get_int_member(o, "id");
+
+        gint id = 0;
+        if (json_object_has_member(o, "id")) {
+            id = json_object_has_member(o, "id");
+        }
 
         FListFriend *friend;
 

commit 55119f8701a97a45826397fb61aef8700c53df87

Fix friend requests regression
b9862d53ddb5f031d3ba6571308788126b3f6223 broke friend request thanks to
a lame confusion betwenn json_object_has_member and
json_object_get_int_member

f-list_friends.c
@@ -515,7 +515,7 @@ static void flist_handle_friends_list(FListAccount *fla, JsonArray *array, FList
 
         gint id = 0;
         if (json_object_has_member(o, "id")) {
-            id = json_object_has_member(o, "id");
+            id = json_object_get_int_member(o, "id");
         }
 
         FListFriend *friend;